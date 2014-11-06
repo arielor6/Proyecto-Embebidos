bba413be0fd8b1b23472f3b61c9e35454214ba20
arielor6 <arielobando@gmail.com>
2014-11-05 20:36:15 -0600

    Update Deteccion_Circulos.cpp
    Se quita por completo la interfaz gráfica de la aplicación.

--- Deteccion_Circulos.cpp
+++ Deteccion_Circulos.cpp
@@ -13,40 +13,11 @@ using namespace cv;
 
 namespace
 {
-    // windows and trackbars name
-    const std::string windowName = "Hough Circle Detection Demo";
-    const std::string cannyThresholdTrackbarName = "Canny threshold";
-    const std::string accumulatorThresholdTrackbarName = "Accumulator Threshold";
-    const std::string usage = "Usage : tutorial_HoughCircle_Demo <path_to_input_image>\n";
-
     // initial and max values of the parameters of interests.
     const int cannyThresholdInitialValue = 200;
     const int accumulatorThresholdInitialValue = 50;
     const int maxAccumulatorThreshold = 200;
     const int maxCannyThreshold = 255;
-
-    void HoughDetection(const Mat& src_gray, const Mat& src_display, int cannyThreshold, int accumulatorThreshold)
-    {
-        // will hold the results of the detection
-        std::vector<Vec3f> circles;
-        // runs the actual detection
-        HoughCircles( src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows/8, cannyThreshold, accumulatorThreshold, 0, 0 );
-
-        // clone the colour, input image for displaying purposes
-        Mat display = src_display.clone();
-        for( size_t i = 0; i < circles.size(); i++ )
-        {
-            Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-            int radius = cvRound(circles[i][2]);
-            // circle center
-            circle( display, center, 3, Scalar(0,255,0), -1, 8, 0 );
-            // circle outline
-            circle( display, center, radius, Scalar(0,0,255), 3, 8, 0 );
-        }
-
-        // shows the results
-        imshow( windowName, display);
-    }
 }
 
 
@@ -81,11 +52,6 @@ int main(int argc, char** argv)
     int cannyThreshold = cannyThresholdInitialValue;
     int accumulatorThreshold = accumulatorThresholdInitialValue;
 
-    // create the main window, and attach the trackbars
-    namedWindow( windowName, WINDOW_AUTOSIZE );
-    createTrackbar(cannyThresholdTrackbarName, windowName, &cannyThreshold,maxCannyThreshold);
-    createTrackbar(accumulatorThresholdTrackbarName, windowName, &accumulatorThreshold, maxAccumulatorThreshold);
-
     // infinite loop to display
     // and refresh the content of the output image
     // until the user presses q or Q
@@ -97,12 +63,13 @@ int main(int argc, char** argv)
         cannyThreshold = std::max(cannyThreshold, 1);
         accumulatorThreshold = std::max(accumulatorThreshold, 1);
 
-        //runs the detection, and update the display
-        HoughDetection(src_gray, src, cannyThreshold, accumulatorThreshold);
+        // will hold the results of the detection
+        std::vector<Vec3f> circles;
+        // runs the actual detection
+        HoughCircles( src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows/8, cannyThreshold, accumulatorThreshold, 0, 0 );
 
         // get user key
         key = waitKey(10);
     }
-
     return 0;
 }
