2bdcd373e2f52d33e5f283dc7a0fb32d24818851
arielor6 <arielobando@gmail.com>
2014-11-08 14:41:12 -0600

    Update deteccion.cpp
    Varios cambios, se agrego una vista para prueba y se ajustaron las constantes de configuracion

--- deteccion.cpp
+++ deteccion.cpp
@@ -3,33 +3,16 @@
  * modificado por: Ariel Obando, Fabio Ugalde, Alejandro Vargas
  */
 
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-
-using namespace std;
-using namespace cv;
-
-namespace
-{
-    // initial values of the parameters of interests.
-    const int cannyThreshold = 200;
-    const int accumulatorThreshold = 50;
-}
-
+#include "Detection.hpp"
 
 int main(int argc, char** argv)
-{
-    Mat src, src_gray;
-
+{    
     // Read the image
     src = imread( "top.jpg", 1 );
 
     if( src.empty() )
     {
         std::cerr<<"Invalid input image\n";
-        std::cout<<usage;
         return -1;
     }
 
@@ -53,10 +36,26 @@ int main(int argc, char** argv)
         // will hold the results of the detection
         std::vector<Vec3f> circles;
         // runs the actual detection
-        HoughCircles( src_gray, circles, HOUGH_GRADIENT, 1, src_gray.rows/8, cannyThreshold, accumulatorThreshold, 0, 0 );
+        HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, min_dist, cannyThreshold, accumulatorThreshold, min_radio, max_radio );
         
         // prints the number of detected circles
-        printf("%d\n", circles.size());
+	int numCircles = circles.size();
+	printf("Personas: %d\n",numCircles);
+
+	  /// Draw the circles detected
+	  for( size_t i = 0; i < circles.size(); i++ )
+	  {
+	      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+	      int radius = cvRound(circles[i][2]);
+	      // circle center
+	      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+	      // circle outline
+	      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
+	   }
+
+	  /// Show your results
+	  namedWindow( "Conteo de Personas", CV_WINDOW_AUTOSIZE );
+	  imshow( "Conteo de Personas", src );
         
         // get user key
         key = waitKey(10);
